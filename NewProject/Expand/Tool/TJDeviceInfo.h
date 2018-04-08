36131e18bcba0aee7abf2520954991a8309b9b75
wlijianqiang <lijianqiang.super@gmail.com>
2018-04-08 15:36:56 +0800

    添加设备信息代码

---
+++ NewProject/Expand/Tool/TJDeviceInfo.h
@@ -0,0 +1,13 @@
+//
+//  TJDeviceInfo.h
+//  NewProject
+//
+//  Created by lijianqiang on 2018/4/8.
+//  Copyright © 2018年 STV. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TJDeviceInfo : NSObject
+
+@end
---
+++ NewProject/Expand/Tool/TJDeviceInfo.m
@@ -0,0 +1,13 @@
+//
+//  TJDeviceInfo.m
+//  NewProject
+//
+//  Created by lijianqiang on 2018/4/8.
+//  Copyright © 2018年 STV. All rights reserved.
+//
+
+#import "TJDeviceInfo.h"
+
+@implementation TJDeviceInfo
+
+@end
